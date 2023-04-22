828f584f54fc1fef9c41e2f7d251f51ac5fbaf03
Brandon <brandonnguyenlam@gmail.com>
2023-04-22 13:27:12 -0500

    ⏰ temp storing files away to get less errors for styling

---
+++ src/temp/CustomBlockDefinitions.tsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import Blockly from "blockly";
+// import 'blockly/python'
+//
+// Blockly.Blocks['start_selenium'] = {
+//     init: function() {
+//         this.appendDummyInput()
+//             .appendField("Start Selenium");
+//         this.setPreviousStatement(true, null);
+//         this.setNextStatement(true, null);
+//         this.setColour(120);
+//         this.setTooltip("");
+//         this.setHelpUrl("");
+//     }
+// };
+//
+// Blockly.Blocks['go_to_link'] = {
+//     init: function() {
+//         this.appendValueInput("url")
+//             .setCheck("String")
+//             .setAlign(Blockly.ALIGN_RIGHT)
+//             .appendField("Go to URL");
+//         this.setPreviousStatement(true, null);
+//         this.setNextStatement(true, null);
+//         this.setColour(230);
+//         this.setTooltip("");
+//         this.setHelpUrl("");
+//     }
+// };
+export default function CustomBlockDefinitions() {
+    return <div>hi</div>
+}
---
+++ src/temp/CustomBlockPython.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import Blockly from "blockly";
+// import 'blockly/python'
+//
+// Blockly.Python['start_selenium'] = function(block) {
+//     var code = 'from selenium import webdriver\n';
+//     code += 'from selenium.webdriver.common.keys import Keys\n';
+//     code += 'from selenium.webdriver.common.by import By\n';
+//     code += 'driver = webdriver.Chrome()\n';
+//     return code;
+// };
+//
+// Blockly.Python['go_to_link'] = function(block) {
+//     var value_url = Blockly.Python.valueToCode(block, 'url', Blockly.Python.ORDER_ATOMIC);
+//     var code = 'driver.get(' + value_url + ')\n';
+//     return code;
+// };
+export default function CustomBlockDefinitions() {
+    return <div>hi</div>
+}
+
